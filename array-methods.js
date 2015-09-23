05e6b636baab59ca2f19a58cea79e210cabf0196
SkinnyFatMan <SkinnyFatMan@gmail.com>
2015-09-23 10:22:16 -1000

    passed a good chunk of the tests

--- array-methods.js
+++ array-methods.js
@@ -5,12 +5,15 @@ var dataset = require('./dataset.json');
   greater than 100000.00
   assign the resulting array to `hundredThousandairs`
 */
-var hundredThousandairs = null;
+var hundredThousandairs = dataset.bankBalances.filter(function(current, index, array) {
+  return current.amount > 100000;
+});
+
 
 /*
   set a new key for each object in bankBalances named `rounded`
   the value of this key will be the `amount` rounded to the nearest dollar
-  example 
+  example
     {
       "amount": "134758.44",
       "state": "HI",
@@ -18,22 +21,33 @@ var hundredThousandairs = null;
     }
   assign the resulting array to `roundedDollar`
 */
-var roundedDollar = null;
+var roundedDollar = dataset.bankBalances.map(function(current, index, array) {
+  var obj = current
+  obj.rounded = Math.round(current.amount);
+  return obj;
+});
 
 /*
   set a the `amount` value for each object in bankBalances
   to the value of `amount` rounded to the nearest 10 cents
-  example 
+  example
     {
       "amount": 134758.4,
       "state": "HI"
     }
   assign the resulting array to `roundedDime`
 */
-var roundedDime = null;
+var roundedDime = dataset.bankBalances.map(function(current, index, array) {
+  var obj = {};
+  obj.amount = Math.round(current.amount * 10) / 10;
+  obj.state = current.state;
+  return obj;
+});
 
 // set sumOfBankBalances to the sum of all amounts in bankBalances
-var sumOfBankBalances = null;
+var sumOfBankBalances = dataset.bankBalances.reduce(function(previous, current, index, array) {
+  return (Math.round(previous * 100)/100) + (Math.round(100 * current.amount) / 100);
+}, 0);
 
 /*
   set sumOfInterests to the sum of the 18.9% interest
@@ -47,8 +61,15 @@ var sumOfBankBalances = null;
     Delaware
   the result should be rounded to the nearest cent
  */
-var sumOfInterests = null;
-
+var sumOfInterests = dataset.bankBalances.filter(function(current, index, array) {
+  var states = [ 'WI', 'IL', 'WY', 'OH', 'GA', 'DE'];
+  return states.indexOf(current.state) !== -1;
+}).map(function(current, index, array) {
+  var interest = current.amount * .189;
+  return interest;
+}).reduce(function(previous, current, index, array) {
+  return Math.round(previous * 100) / 100 + Math.round(current * 100) / 100;
+}, 0);
 /*
   set sumOfHighInterests to the sum of the 18.9% interest
   for all amounts in bankBalances
@@ -63,7 +84,27 @@ var sumOfInterests = null;
     Delaware
   the result should be rounded to the nearest cent
  */
-var sumOfHighInterests = null;
+var reduced = dataset.bankBalances.filter(function(current, index, array) {
+  var states = [ 'WI', 'IL', 'WY', 'OH', 'GA', 'DE'];
+  return states.indexOf(current.state) == - 1;
+}).reduce(function(previous, current, index, array) {
+  var newList = previous ? previous : {};
+  newList[current.state] = newList[current.state] ? Number(newList[current.state]) : 0;
+  newList[current.state] += Number(current.amount);
+  return newList;
+}, false);
+var newRay = [];
+for ( k in reduced) {
+  newRay.push({ k : reduced[k]});
+};
+var sumOfHighInterests = newRay.map(function(current, index, array) {
+  var interest = current.k * .189;
+  return interest;
+}).filter(function(current, index, array) {
+  return current > 50000;
+}).reduce(function(previous, current, index, array) {
+  return Math.round(previous * 100) / 100 + Math.round(current * 100) / 100;
+}, 0);
 
 /*
   aggregate the sum of bankBalance amounts
@@ -77,14 +118,14 @@ var stateSums = null;
 
 /*
   set lowerSumStates to an array containing
-  only the two letter state abbreviation of each state 
+  only the two letter state abbreviation of each state
   where the sum of amounts in the state is
     less than 1,000,000
  */
 var lowerSumStates = null;
 
 /*
-  set higherStateSums to be the sum of 
+  set higherStateSums to be the sum of
     all amounts of every state
     where the sum of amounts in the state is
       greater than 1,000,000
